98fd072ef0c9690786abf324650c5809c5c3d04d
Henryk Konsek <hekonsek@gmail.com>
2019-01-25 14:17:45 +0100

    x

--- app.sh
+++ app.sh
@@ -2,5 +2,5 @@
 # This script is configured in cdk.json to be used to execute
 # the CDK java app by the command-line toolkit.
 # The file .classpath.txt is created by when `mvn package` is called
-# The first argument will be used as argv[0]xxxx
+# The first argument will be used as argv[0]xxxxxx
 exec java -cp target/classes:$(cat .classpath.txt) com.myorg.HelloApp hello-cdk $@
